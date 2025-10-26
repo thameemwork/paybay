32ccb0fbd454030def153489547731ffa5441c7f
thameemwork <athameem35@gmail.com>
2025-10-26 19:32:02 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react(), tailwindcss()],
+   base: process.env.VITE_BASE_PATH || "/paybay",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
