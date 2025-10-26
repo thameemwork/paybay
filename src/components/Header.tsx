fedcb2ee1ceed21dde3e51151513e7d729213553
thameemwork <athameem35@gmail.com>
2025-10-26 19:35:25 +0530

    Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 
-import React from "react";
 import { Bell, ChevronDown, Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
